fec987018fe762585424ae4755f111d4108edf65
Harishsoma <harishkumar.soma@vitesse-group.com>
2020-02-28 07:56:06 -0600

    Initial commit

---
+++ src/main/java/com/vitesse/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,26 @@
+package com.vitesse.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.vitesse.gatewayservice.filters.AddRequestHeaderFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@Configuration
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	@Bean
+	public AddRequestHeaderFilter addRequestHeaderFilter() {
+		return new AddRequestHeaderFilter();
+	}
+
+}
---
+++ src/main/java/com/vitesse/gatewayservice/filters/AddRequestHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.vitesse.gatewayservice.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AddRequestHeaderFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		ctx.addZuulRequestHeader("x-location","USA");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
